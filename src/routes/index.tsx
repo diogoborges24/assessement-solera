1e67f9fac5c405587a3c8a3deb13e16dd476510e
diogoborges24 <diogoborges24@gmail.com>
2022-12-01 22:05:13 +0100

    Mui install

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 
 import{Routes, Route, Navigate} from 'react-router-dom'; 
+import Button from '@mui/material/Button';
 
 //approutes pois o reactrouterdom tem um componente routes causa conflito
 export const AppRoutes =() =>{
 
     return(
     <Routes>
-        <Route path="/init-page" element={<p>Inital Page</p>} />
+        <Route path="/init-page" element={<Button>teste</Button>} />
 
         <Route path="*" element={<Navigate to="/init-page"/>} /> 
     </Routes>);
