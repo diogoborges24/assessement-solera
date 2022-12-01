624c0c84473cf57641ad0ae792360475de67b8b7
diogoborges24 <diogoborges24@gmail.com>
2022-12-01 21:50:37 +0100

    Fix Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export{};
+
+import{Routes, Route, Navigate} from 'react-router-dom'; 
+
+//approutes pois o reactrouterdom tem um componente routes causa conflito
+export const AppRoutes =() =>{
+
+    return(
+    <Routes>
+        <Route path="/init-page" element={<p>Inital Page</p>} />
+
+        <Route path="*" element={<Navigate to="/init-page"/>} /> 
+    </Routes>);
+}
